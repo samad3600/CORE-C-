3f4f2d688d197e6bb13e43493b96808cb59ff901
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-08-25 21:38:34 +0530

    Arrays.cpp

---
+++ Arrays.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namspace std;
+
+int main() {
+  int mark1,mark2,mark3,mark4;
+int arr[7];
+int arr2[10] = {1,2,3,4};
+int arr3[] = {1,2,3,4,5,6};
+cout << arr2[] << endl;
+cout << sizeof(arr[0]) << endl;
+cout << sizeof(arr3) << endl;
+return 0;
+}
