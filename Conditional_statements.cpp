04c06cd3ec6dcf947bc0fc6dceff103b6be50947
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-08-26 23:05:26 +0530

    Conditional_statements.cpp

---
+++ Conditional_statements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    if (number >= 0) {
+        cout << "You entered a positive integer: " << number << endl;
+    } else {
+        cout << "You entered a negative integer: " << number << endl;
+    }
+
+    cout << "This line is always printed." << endl;
+    return 0;
+}
