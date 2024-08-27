4957dbaa595263f1552c615df6a10141c37432e0
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-08-27 22:32:20 +0530

    Input_Output.cpp

---
+++ Input_Output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "This is C++ Programming";
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;  // Taking input
+    cout << "The number is: " << num;
+    return 0;
+}
