401350aed82b688511dd155cd83b9bbbe9cb633d
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-09-07 22:48:39 +0530

    Operator_Overloading.cpp

---
+++ Operator_Overloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    int real, imag;
+public:
+    // Constructor
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    // Overloading the + operator
+    Complex operator + (const Complex& obj) {
+        Complex res;
+        res.real = real + obj.real;
+        res.imag = imag + obj.imag;
+        return res;
+    }
+
+    void print() {
+        cout << real << " + i" << imag << endl;
+    }
+};
+
+int main() {
+    Complex c1(10, 5), c2(2, 4);
+    Complex c3 = c1 + c2; // Calls the overloaded + operator
+    c3.print();
+    return 0;
+}
