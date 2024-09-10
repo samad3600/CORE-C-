c9bbcae8371c1779e913d9aaf7d4370ebc5120cd
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-09-10 23:37:59 +0530

    Encapsulation.cpp

---
+++ Encapsulation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+private:
+    int salary;
+
+public:
+    void setSalary(int s) {
+        salary = s;
+    }
+
+    int getSalary() {
+        return salary;
+    }
+};
+
+int main() {
+    Employee emp;
+    emp.setSalary(50000);
+    cout << "Salary: " << emp.getSalary() << endl;
+    return 0;
+}
