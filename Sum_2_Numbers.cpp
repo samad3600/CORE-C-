a55aed8ad885085fe53804315b5d42a315c46857
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-09-30 23:25:48 +0530

    Sum_2_Numbers.cpp

---
+++ Sum_2_Numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "Sum: " << num1 + num2 << endl;
+    return 0;
+}
